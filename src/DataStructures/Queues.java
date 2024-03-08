a02f89e455886e33c12b4f6ba96b61f86857afa3
Yang Enmanuel <enmanuelcalzat@gmail.com>
2024-03-08 17:04:48 -0300

    fixed warning

--- src/DataStructures/Queues.java
+++ src/DataStructures/Queues.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class Queues {
     public static void main(String[] args) {
 
-        Queue<String> queue = new LinkedList<String>();
+        Queue<String> queue = new LinkedList<>();
 
         queue.offer("first");
         queue.offer("second");
